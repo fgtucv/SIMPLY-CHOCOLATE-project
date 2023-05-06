781540299f5d9c0ab1d03ce4aa09103b705d6da4
SolomiaOstruk <110992987+SolomiaOstruk@users.noreply.github.com>
2023-05-06 19:45:31 +0300

    modal-header

---
+++ src/js/modal-3.js
@@ -0,0 +1,20 @@
+(() => {
+    const refs = {
+        openModalBtn: document.querySelector("[data-open-modal3]"),
+        closeModalBtn: document.querySelector("[data-close-modal3]"),
+        backdrop: document.querySelector("[data-backdrop3]"),
+    };
+
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+
+    refs.backdrop.addEventListener("click", logBackdropClick);
+
+    function toggleModal() {
+        refs.backdrop.classList.toggle("is-hidden3");
+    }
+
+    function logBackdropClick() {
+        console.log("Це клік в бекдроп");
+    }
+})();
