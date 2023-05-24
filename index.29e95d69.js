62bcbb6bf469f7016a63c9d159ae7a77fbbaf1c4
fgtucv <109958193+fgtucv@users.noreply.github.com>
2023-05-24 15:14:35 +0000

    Deploying to gh-pages from @ fgtucv/SIMPLY-CHOCOLATE-project@9bf79ce480d76e1be94fdb7d742c9ee8509edf80 🚀

---
+++ index.29e95d69.js
@@ -0,0 +1,2 @@
+new Swiper(".mySwiper",{slidesPerView:1,spaceBetween:10,pagination:{el:".swiper-pagination",clickable:!0},breakpoints:{0:{slidesPerView:1,spaceBetween:0},768:{slidesPerView:2.5,spaceBetween:18},1200:{slidesPerView:4,spaceBetween:18}}});
+//# sourceMappingURL=index.29e95d69.js.map
