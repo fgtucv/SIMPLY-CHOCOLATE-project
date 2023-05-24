9418e9ad1b5e8c5b18f126cc0d54d809a08fbc9c
fgtucv <oles.slava23@gmail.com>
2023-05-24 18:13:37 +0300

    G

--- src/js/modal-2.js
+++ src/js/modal-2.js
@@ -3,15 +3,21 @@
         openModalBtn: document.querySelector("[data-open-modal2]"),
         closeModalBtn: document.querySelector("[data-close-modal2]"),
         backdrop: document.querySelector("[data-backdrop2]"),
+        sendBtn: document.querySelector("[data-send]"),
     };
 
     refs.openModalBtn.addEventListener("click", toggleModal);
     refs.closeModalBtn.addEventListener("click", toggleModal);
+    refs.sendBtn.addEventListener("click", closeModal);
 
     refs.backdrop.addEventListener("click", logBackdropClick);
 
     function toggleModal() {
         refs.backdrop.classList.toggle("is-hidden2");
+    }
+
+    function closeModal() {
+        refs.backdrop.classList.toggle("is-close");
     }
 
     function logBackdropClick() {
