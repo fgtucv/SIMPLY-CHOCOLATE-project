c486134df35fbcf7c69020885bb9ae9f9bf5b06d
fgtucv <109958193+fgtucv@users.noreply.github.com>
2023-06-06 09:36:33 +0000

    Deploying to gh-pages from @ fgtucv/SIMPLY-CHOCOLATE-project@6e67e0a7565c8c976d9025e03c6592e28a81d4f8 🚀

---
+++ index.4a63c38a.js
@@ -0,0 +1,2 @@
+window.onscroll=function(){document.body.scrollTop>20||document.documentElement.scrollTop>20?document.getElementById("scrollTopBtn").style.display="block":document.getElementById("scrollTopBtn").style.display="none"};
+//# sourceMappingURL=index.4a63c38a.js.map
