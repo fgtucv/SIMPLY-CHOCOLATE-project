01a196d229e28b3527b6ebd13792dc7b0580cfe1
fgtucv <109958193+fgtucv@users.noreply.github.com>
2023-06-09 15:35:10 +0000

    Deploying to gh-pages from @ fgtucv/SIMPLY-CHOCOLATE-project@0cbbaa0cf75894cc27af01b73de631723a415369 🚀

---
+++ index.e8aab5aa.js
@@ -0,0 +1,2 @@
+(()=>{let e={openModalBtn:document.querySelector("[data-open-modal6]"),closeModalBtn:document.querySelector("[data-close-modal6]"),backdrop:document.querySelector("[data-backdrop6]")};function o(){e.backdrop.classList.toggle("is-hidden6")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o),e.backdrop.addEventListener("click",function(){console.log("Це клік в бекдроп")})})();
+//# sourceMappingURL=index.e8aab5aa.js.map
---
+++ index.ff042468.js
@@ -0,0 +1,2 @@
+(()=>{let e={openModalBtn:document.querySelector("[data-open-modal2]"),closeModalBtn:document.querySelector("[data-close-modal2]"),backdrop:document.querySelector("[data-backdrop2]")};function o(){e.backdrop.classList.toggle("is-hidden2")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o),e.backdrop.addEventListener("click",function(){console.log("Це клік в бекдроп")})})();
+//# sourceMappingURL=index.ff042468.js.map
